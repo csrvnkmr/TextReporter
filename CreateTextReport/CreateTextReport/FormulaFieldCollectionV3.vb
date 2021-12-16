1c961b15970f54f72c50c75b49c2bd941a9a8ff0
csrvnkmr <96159739+csrvnkmr@users.noreply.github.com>
2021-12-16 09:45:51 +0530

    Delete CreateTextReport/CreateTextReport directory

--- CreateTextReport/CreateTextReport/FormulaFieldCollectionV3.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class FormulaFieldCollectionV3
-    Inherits List(Of FormulaFieldV3)
-    Public Sub SetDataRow(pDR As DataRow)
-        For i = 0 To Count - 1
-            Item(i).DR = pDR
-        Next
-    End Sub
-End Class
